cb5dd9365fe883ab100e26157f61a4dfebf8eb14
Wolflbtw <93408674+Wolflbtw@users.noreply.github.com>
2021-11-14 09:35:55 +0100

    Update main.py

--- watch time bot/main.py
+++ watch time bot/main.py
@@ -1,8 +1,8 @@
 import time
 from selenium import webdriver
 
-video_link = 'https://www.youtube.com/watch?v=uloK1HOTL1c'
-views = 100
+video_link = 'https://www.youtube.com/watch?v=Bew6HVHATm8'
+views = 247
 video_duration = 7 * 60
 
 driver = webdriver.Chrome()
